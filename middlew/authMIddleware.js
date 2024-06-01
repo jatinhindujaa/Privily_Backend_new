f2db7e8176c1cf3bd48d85286862753542fdda68
Sourabh Saraswat 191939 <saraswatsourabh5@gmail.com>
2024-06-02 00:26:55 +0530

    block user changes

--- middlew/authMIddleware.js
+++ middlew/authMIddleware.js
@@ -11,6 +11,9 @@ const authMiddleware = asyncHandler(async (req, res, next) => {
         const decoded = jwt.verify(token, process.env.JWT_SECRET || "");
         const user = await User.findById(decoded?.id);
         req.user = user;
+        if (req.user.isBlocked){
+          throw new Error("Blocked User is trying to login")
+        }
         next();
       }
     } catch (error) {
